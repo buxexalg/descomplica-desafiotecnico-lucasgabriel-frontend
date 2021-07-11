7ec62ff0cfc2af56b226897901241b73e82cc08a
buxexalg <lucasgralcantara@gmail.com>
2021-07-11 14:33:33 -0300

    refactor: now the CPF is formatted

--- src/components/StudentRow/StudentRow.tsx
+++ src/components/StudentRow/StudentRow.tsx
@@ -12,11 +12,19 @@ interface Item {
 	email: string;
 }
 
+function formatCPF(cpf: string) {
+	cpf = cpf.replace(/[^\d]/g, '');
+
+	return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+}
+
 function StudentRow(props: studentRow) {
+	const formattedCpf = formatCPF(props.item.cpf);
+
 	return (
 		<tr className="studentRow" key={props.key}>
 			<td className="left">{props.item.name}</td>
-			<td className="center">{props.item.cpf}</td>
+			<td className="center">{formattedCpf}</td>
 			<td className="right">{props.item.email}</td>
 		</tr>
 	);
