b71391dd9bf633af46ea16254551103bd7b97e03
buxexalg <lucasgralcantara@gmail.com>
2021-07-11 12:40:32 -0300

    feat: add global variable

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import Homepage from './pages/homepage/Homepage';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+	uri: (process.env.REACT_APP_API_URL || 'http://localhost:4000') + '/graphql',
 	cache: new InMemoryCache(),
 });
 
