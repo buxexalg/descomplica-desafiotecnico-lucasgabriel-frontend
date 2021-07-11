6e2487bd8b17d23311716182d70d653e48073e53
buxexalg <lucasgralcantara@gmail.com>
2021-07-11 12:06:40 -0300

    feat: add searchButton to a component

---
+++ src/components/SearchButton/SearchButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+function SearchButton() {
+	return (
+		<button className="searchStudentButton" type="submit">
+			Consultar
+		</button>
+	);
+}
+
+export default SearchButton;
